9d823165b8e917d966e8aea00a063d141a133d80
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-12-09 05:08:34 +0000

    Checking in changes prior to tagging of version 0.04.  Changelog diff is:
    === Changes
    ==================================================================
    --- Changes	(revision 36741)
    +++ Changes	(local)
    @@ -1,5 +1,10 @@
     Revision history for Perl extension CGI::ExceptionManager
     
    +0.04
    +
    +    - CGI::ExceptionManager->run returns collback's return value
    +      or detach()'s argument
    +
     0.03
     
         - fix generation of bogus error message when error is propagated from a eval(by kazuho++)
    
    
    git-svn-id: http://svn.coderepos.org/share/lang/perl/CGI-ExceptionManager/trunk@26179 d0d07461-0603-4401-acd4-de1884942a52

--- lib/CGI/ExceptionManager.pm
+++ lib/CGI/ExceptionManager.pm
@@ -2,7 +2,7 @@ package CGI::ExceptionManager;
 use strict;
 use warnings;
 use 5.00800;
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 sub detach { die bless [@_], 'CGI::ExceptionManager::Exception' }
 
